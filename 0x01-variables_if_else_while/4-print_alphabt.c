25b281ce59c962d0e3417671adc868074df08fae
JuneSquary <Squarymt@gmail.com>
2023-06-18 14:01:37 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,19 +7,29 @@
 int main(void)
 {
 char ch = 'a';
-
 while (ch <= 'z')
-
-{      
+{
 
 if (ch != 'e' && ch != 'q')
 
-{      
+{
+
 
 putchar(ch);
+
+}
+
+
+ch++;
+
+
+
+
 }       
-ch++;   
-}       
+
 putchar('\n');
+
+
 return (0);
+
 }
