0bc510a02d5ba268a9d853af01393585f3f60241
JuneSquary <Squarymt@gmail.com>
2023-06-20 17:08:06 -0700

    Readme file created

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - make the alphabet
+ *
+ * Return:void
+ */
+
+void print_alphabet(void)
+{
+char ch;
+for (cha m 'a'; ch <m 'z'; ch++)
+_putchar(cha);
+_putchar ('\n');
+}
