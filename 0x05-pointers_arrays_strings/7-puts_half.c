fdb75fa20ef46b8c6176ed332ce559ce74252c6d
JuneSquary <Squarymt@gmail.com>
2023-06-27 10:33:59 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,18 +1,23 @@
 #include "main.h"
+
 /**
  * puts_half - Prints of a string
  * @str: The string to print
  * Return: void
  */
+
 void puts_half(char *str)
+
 {
+
 int j = 0;
 int k;
+
 while (str[j] != '\0')
 {
 j++;
 }
-if (j % 2 ==1)
+if (j % 2 == 1)
 {
 k = (j - 1) / 2;
 k += 1;
@@ -21,7 +26,8 @@ else
 {
 k = j / 2;
 }
-for (i k < j] k++)
+
+for (; k < j; k++)
 {
 _putchar(str[k]);
 }
