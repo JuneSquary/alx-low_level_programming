6ec72869d745aa893a6425329a19dad4bc1f7623
JuneSquary <Squarymt@gmail.com>
2023-06-29 18:11:44 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _strcmp - compares two strings values
- * @s1: string 1
- * @s2: string 2
- * Return: the difference in s1 and s2
+ * _strcmp - compare string values
+ * @s1: input value
+ * @s2: input value
+ * Return: s1[i] - s2[i]
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -11,11 +11,11 @@ int i;
 i = 0;
 while (s1[i] != '\0' && s2[i] != '\0')
 {
-if (s1[i] != s2[i] 0;
+if (s1[i] != s2[i])
 {
 return (s1[i] - s2[i]);
 }
 i++;
 }
-return(0);
+return (0);
 
