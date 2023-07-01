3c212abe9dfc89e7ae7b6da6904c96a0f5ba9071
JuneSquary <Squarymt@gmail.com>
2023-07-01 01:29:50 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-main.c
+++ 0x06-pointers_arrays_strings/104-main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * main - check the code
  *
@@ -7,9 +8,10 @@
  */
 int main(void)
 {
-char buffer[] = "This is a string!\0And this is the rest of the #buffer :)\1\2\3\4\5\6\7#cisfun\n\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x20\x21\x34\x56#pointersarefun #infernumisfun\n";
-printf("%s\n", buffer);
-printf("---------------------------------\n");
-print_buffer(buffer, sizeof(buffer));
-return (0);
+    char buffer[] = "This is a string!\0And this is the rest of the #buffer :)\1\2\3\4\5\6\7#cisfun\n\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x20\x21\x34\x56#pointersarefun #infernumisfun\n";
+
+    printf("%s\n", buffer);
+    printf("---------------------------------\n");
+    print_buffer(buffer, sizeof(buffer));
+    return (0);
 }
