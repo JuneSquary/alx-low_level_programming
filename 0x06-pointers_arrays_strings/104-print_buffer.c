411c06b07450f606c449a88f4669da98f8b0e437
JuneSquary <Squarymt@gmail.com>
2023-07-01 15:44:30 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,41 +1,50 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- *print_buffer - a program that prints a buffer
- *@b: buffer to print
- *@size: buffer's size
- *Return: nothing
+ * print_buffer - prints buffer
+ * @b: buffer
+ * @size: size
+ * Return: void
  */
+
 void print_buffer(char *b, int size)
 {
-int j, k, l;
-if (size <= 0)
-printf("\n");
-else
-{
-for (j = 0; j < size; j += 10)
-{
-printf("%.8x:", j);
-for (k = j; k < j + 10; k++)
-{
-if (k % 2 == 0)
-printf(" ");
-if (k < size)
-printf("%.2x", *(b + k));
-else
-printf(" ");
-}
-printf(" ");
-for (l = j; l < j + 10; l++)
-{
-if (l >= size)
-break;
-if (*(b + l) < 32 || *(b + l) > 126)
-printf("%c", '.');
-else
-printf("%c", *(b + l));
-}
-printf("\n");
-}
-}
+	int k, m, l;
+
+	k = 0;
+
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	while (k < size)
+	{
+		m = size - k < 10 ? size - k : 10;
+		printf("%08x: ", k);
+		for (l = 0; l < 10; l++)
+		{
+			if (l < m)
+				printf("%02x", *(b + k + l));
+			else
+				printf("  ");
+			if (l % 2)
+			{
+				printf(" ");
+			}
+		}
+		for (l = 0; l < m; l++)
+		{
+			int c = *(b + k + l);
+
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		k += 10;
+	}
 }
