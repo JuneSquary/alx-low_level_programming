ba25494dcc2e046ea3199a348496dd177d5ae0f7
JuneSquary <Squarymt@gmail.com>
2023-07-04 04:18:42 -0700

    0-memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,7 +3,7 @@
  * _memset - Entry point
  * @s: pointed destination
  * @b: constant byte
- * @an: bytes
+ * @n: bytes
  * Return: Always 0 (Success)
  */
 char *_memset(char *s, char b, unsigned int n)
